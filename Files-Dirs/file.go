34b89702fb72ea9658b4ecdfdc5cd8f2a11ea7c5
4m4z1gh <aitbaha48@gmail.com>
2024-07-14 02:38:38 +0300

    --

--- Files-Dirs/file.go
+++ Files-Dirs/file.go
@@ -5,14 +5,21 @@ import (
 	"os"
 )
 
+func write(file *os.File, data string) {
+	file.WriteString(data)
+}
+
 func main() {
 
 	//Create the file
 	emptyFile, err := os.Create("file.txt")
 	defer emptyFile.Close()
+	emptyFile.WriteString("hello world")
 	if err != nil {
 		//panic("Could Not Create file.txt.")
 	}
+
+	write(emptyFile, "hello world")
 
 	state, er := os.Stat("renamedFile.txt")
 	fmt.Print("result : ", os.IsNotExist(er), state.ModTime())
