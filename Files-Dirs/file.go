789257c63a4a48b907edde9438df0c09bfd3cbe3
4m4z1gh <aitbaha48@gmail.com>
2024-07-14 02:30:17 +0300

    --

--- Files-Dirs/file.go
+++ Files-Dirs/file.go
@@ -3,15 +3,23 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
 
+	//Create the file
 	emptyFile, err := os.Create("file.txt")
 	defer emptyFile.Close()
 	if err != nil {
-		panic("Could Not Create file.txt.")
+		//panic("Could Not Create file.txt.")
 	}
+
+	state, _ := os.Stat("renamedFile.txt")
+	fmt.Print(state.ModTime().Add(time.Hour))
+	return
+	//Rename The file name
+	os.Rename("file.txt", "renamedFile.txt")
 	fmt.Println(emptyFile)
 
 }
