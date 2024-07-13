e5ab8fecbf5b457b0b8d85c345c9cc1cd8beb10b
4m4z1gh <aitbaha48@gmail.com>
2024-07-14 01:16:07 +0300

    --

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		ch1 <- "Hello"
+	}()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch2 <- "World"
+	}()
+
+	select {
+	case msg1 := <-ch1:
+		fmt.Println(msg1)
+	case msg2 := <-ch2:
+		fmt.Println(msg2)
+	}
+}
