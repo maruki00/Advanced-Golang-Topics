d76f694005eabee6b71650bacb15d25fe56ab9c1
maruki00 <aitbaha48@gmail.com>
2024-08-31 21:13:50 +0300

    --

--- socket/client.go
+++ socket/client.go
@@ -1,44 +1,60 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
 
 func main() {
+	name := flag.String("name", "anonymous", "This ur nikename")
+	flag.Parse()
+
 	// Connect to the server
 	conn, err := net.Dial("tcp", "localhost:8080")
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
+
 	defer conn.Close()
 
 	for {
-		_, err = conn.Write([]byte("hello world"))
+		_, err = conn.Write([]byte(*name))
 		if err != nil {
-			fmt.Println("error sending data")
+			//fmt.Println("error sending data ", err.Error())
+			panic(err.Error())
 		}
 
-		handleServer(conn)
+		fmt.Print(*name)
+		go handleServer(conn, *name)
 	}
 
 }
 
-func handleServer(conn net.Conn) {
+func handleServer(conn net.Conn, nName string) {
 	defer conn.Close()
+	// fmt.Println(nName)
 	buffer := make([]byte, 1024)
-
+	return
 	for {
 		// Read data from the client
 		n, err := conn.Read(buffer)
 		if err != nil {
-			return
+			panic(err.Error())
 		}
-
 		// Process and use the data (here, we'll just print it)
 		fmt.Printf("Received: %s\n", buffer[:n])
+		var msg string
+		_, err = fmt.Scan(msg)
+		if err != nil {
+			continue
+		}
+		conn.Write([]byte(msg))
+		var m []byte
+		_, _ = fmt.Scan(m)
+		fmt.Println(m)
 	}
 
-	_, _ = conn.Write([]byte("recived thanks ."))
+	// _, _ = conn.Write([]byte("recived thanks ."))
 }
--- socket/server.go
+++ socket/server.go
@@ -3,35 +3,107 @@ package main
 import (
 	"fmt"
 	"net"
+	"time"
 )
 
 type Pair struct {
-	Nikname string
-	Conn net.Conn
-	Addr net.Addr
+	Nikname  string
+	Conn     net.Conn
+	isOnline bool
 }
 
 type Server struct {
-	onlines map[string]bool
-	pairs map[string][2]Pair
+	pairs map[string]Pair
+	rooms map[string]Pair
 }
 
-
-func NewServer() *Server{
-
-}
-
-func NewPair(nikename string, conn net.Conn, addr net.Addr) *Pair{
-	return &Pair{
-		Nikname : nikename,
-		Conn : con,
-		Addr : addr,
+func NewServer() *Server {
+	return &Server{
+		pairs: make(map[string]Pair, 0),
+		rooms: make(map[string]Pair, 0),
 	}
 }
 
+func NewPair(nikename string, conn net.Conn) *Pair {
+	return &Pair{
+		Nikname:  nikename,
+		Conn:     conn,
+		isOnline: true,
+	}
+}
+
+func (s *Server) JoinChat(pair1 Pair, pair2 Pair) error {
+	if pair, ok := s.pairs[pair1.Nikname]; !ok || !pair.isOnline {
+		return fmt.Errorf("the pair %s offline for now, or doesnt exists", pair1.Nikname)
+	}
+
+	if pair, ok := s.pairs[pair2.Nikname]; !ok || !pair.isOnline {
+		return fmt.Errorf("the pair %s offline for now, or doesnt exists", pair2.Nikname)
+	}
+
+	_, ok := s.rooms[pair1.Nikname]
+	if ok {
+		return fmt.Errorf("%s already connected ", pair1.Nikname)
+	}
+	_, ok = s.rooms[pair2.Nikname]
+	if ok {
+		return fmt.Errorf("%s already connected ", pair2.Nikname)
+	}
+
+	s.rooms[pair1.Nikname] = pair2
+	s.rooms[pair2.Nikname] = pair1
+
+	return nil
+}
+
+func (s *Server) Logout(pair1 Pair) {
+	if pair, ok := s.pairs[pair1.Nikname]; ok {
+		pair.isOnline = false
+	}
+}
+
+func (s *Server) Login(pair1 Pair) {
+
+	if pair, ok := s.pairs[pair1.Nikname]; ok {
+		pair.isOnline = true
+	}
+}
+
+func (s *Server) SendMessage(from, to Pair, message string) error {
+
+	if pair, ok := s.rooms[from.Nikname]; pair != to || !ok {
+		return fmt.Errorf("you re not connected to that pair")
+	}
+
+	if _, ok := s.pairs[to.Nikname]; !ok {
+		delete(s.rooms, from.Nikname)
+		return fmt.Errorf("%s are offline ", to.Nikname)
+	}
+
+	msg := fmt.Sprintf("from [%s] : %s", from.Nikname, message)
+	to.Conn.Write([]byte(msg))
+	return nil
+}
+
+func (s *Server) Register(pair Pair) error {
+
+	if _, ok := s.pairs[pair.Nikname]; ok {
+		return fmt.Errorf("pair %s alredy exists", pair.Nikname)
+	}
+
+	s.pairs[pair.Nikname] = pair
+	return nil
+}
 
 func main() {
-	onlines := map[string]bool,0)
+
+	// commands := []string{
+	// 	"join",
+	// 	"register",
+	// 	"logout",
+	// 	"send",
+	// }
+	s := NewServer()
 	// Listen for incoming connections
 	listener, err := net.Listen("tcp", "localhost:8080")
 	if err != nil {
@@ -43,20 +115,22 @@ func main() {
 	fmt.Println("Server is listening on port 8080")
 
 	for {
-		// Accept incoming connections
+
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error:", err)
 			continue
 		}
-
-		// Handle client connection in a goroutine
-		go handleClient(conn)
+		var nikname []byte
+		conn.Read(nikname)
+		conn.Write([]byte("Welcome " + string(nikname)))
+		go s.handleClient(conn)
 	}
 }
 
-func handleClient(conn net.Conn) {
+func (s *Server) handleClient(conn net.Conn) {
 	defer conn.Close()
+
 	buffer := make([]byte, 1024)
 
 	for {
@@ -66,8 +140,32 @@ func handleClient(conn net.Conn) {
 			return
 		}
 
-		// Process and use the data (here, we'll just print it)
-		fmt.Printf("Received: %s\n", buffer[:n])
+		var pair2 chan Pair
+
+		nikname := string(buffer[:n])
+		pair, ok := s.pairs[nikname]
+		if !ok {
+			p := NewPair(nikname, conn)
+			s.Register(*p)
+		}
+		go s.GetOnlinePair(pair2)
+		s.JoinChat(pair, <-pair2)
+	}
+
+}
+
+func (s *Server) GetOnlinePair(pair2 chan Pair) {
+
+	for {
+		for _, pair := range s.pairs {
+			if pair.isOnline {
+				pair2 <- pair
+				return
+
+			}
+		}
+		time.Sleep(time.Microsecond * 100)
+
 	}
 
 }
