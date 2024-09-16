779dc10596eec8bcd7d6c86d03763cf7c3b32756
maruki00 <aitbaha48@gmail.com>
2024-09-17 02:05:31 +0300

    ==

--- CQRS/cmd/main.go
+++ CQRS/cmd/main.go
@@ -1,39 +1,21 @@
 package main
 
 import (
+	"CRQS-GO/internal/commands"
+	"CRQS-GO/internal/queries"
+	"CRQS-GO/internal/repository"
 	"fmt"
-	"sync"
 )
 
-// User is a simple model representing a user
-
-
-
-// Command Handlers (Write-side)
-
-// CreateUserCommand represents a command to create a new user
-
-
-// HandleCreateUserCommand handles the logic for creating a user
-
-
-// Query Handlers (Read-side)
-
-// GetUserQuery represents a query to get user data by ID
-
-
-// HandleGetUserQuery handles the logic for fetching a user by ID
-
-
 func main() {
-	repo := NewRepository()
+	repo := repository.NewRepository()
 
 	// Command: Create a user
-	createCmd := CreateUserCommand{ID: 1, Name: "John Doe"}
+	createCmd := commands.CreateUserCommand{ID: 1, Name: "John Doe"}
 	repo.HandleCreateUserCommand(createCmd)
 
 	// Query: Fetch the user by ID
-	query := GetUserQuery{ID: 1}
+	query := queries.GetUserQuery{ID: 1}
 	user, err := repo.HandleGetUserQuery(query)
 	if err != nil {
 		fmt.Println("Error:", err)
