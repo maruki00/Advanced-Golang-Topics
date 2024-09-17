c7c41a0bc7d64359ddeffa75d926a6278bf40dab
maruki00 <aitbaha48@gmail.com>
2024-09-17 19:55:39 +0300

    ==

---
+++ dependencyInjection/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"database/sql"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	db, err := sql.Open("postgres", "")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	userHandler := user.Wire(db)
+	http.Handle("/user", userHandler.FetchByUsername())
+	http.ListenAndServe(":8000", nil)
+}
