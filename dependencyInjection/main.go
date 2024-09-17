4913bcd1328c5fc40411570579f1080aa891987d
maruki00 <aitbaha48@gmail.com>
2024-09-17 16:19:53 +0300

    ==

--- dependencyInjection/main.go
+++ dependencyInjection/main.go
@@ -1,14 +1,42 @@
 package main
 
-import "github.com/google/wire"
+import (
+   "fmt"
+)
 
-type User struct {
+type Message string
+type Greeter struct {
+   Message Message
+}
+type Event struct {
+   Greeter Greeter
 }
 
-func InitUser() User {
-	wire.Build(NewUser, NewUserName)
+func GetMessage() Message {
+   return Message("Hello world!")
+}
+
+func GetGreeter(m Message) Greeter {
+   return Greeter{Message: m}
+}
+
+func (g Greeter) Greet() Message {
+   return g.Message
+}
+
+func GetEvent(g Greeter) Event {
+	return Event{Greeter: g}
+}
+
+func (e Event) Start() {
+   msg := e.Greeter.Greet()
+   fmt.Println(msg)
 }
 
 func main() {
+   message := GetMessage()
+   greeter := GetGreeter(message)
+   event := GetEvent(greeter)
 
+   event.Start()
 }
