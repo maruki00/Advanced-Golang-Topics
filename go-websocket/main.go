b0ab43bfd27ae5c57aaf2162fec99fdd2457e635
maruki00 <aitbaha48@gmail.com>
2024-11-30 17:42:52 +0100

    --

---
+++ go-websocket/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+// Upgrader is used to upgrade HTTP connections to WebSocket connections.
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	// Upgrade the HTTP connection to a WebSocket connection
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Error upgrading:", err)
+		return
+	}
+	defer conn.Close()
+	// Listen for incoming messages
+	for {
+		// Read message from the client
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Error reading message:", err)
+			break
+		}
+		fmt.Printf("Received: %s\\n", message)
+		// Echo the message back to the client
+		if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
+			fmt.Println("Error writing message:", err)
+			break
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", wsHandler)
+	fmt.Println("WebSocket server started on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
---
+++ wire/go.mod
@@ -0,0 +1,5 @@
+module wire-go
+
+go 1.22.5
+
+require github.com/google/wire v0.6.0 // indirect
